1cceb6166d5ab440b21c6f7705c763cbd9234ae7
Christiaan Quyn <christianq010@gmail.com>
2017-05-04 23:05:25 +0530

    feat: Install Gulp via npm and add gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,7 @@
+//Create a variable named gulp to reference our gulp files in the node_modules folder when installed
+var gulp = require('gulp');
+
+//after each task/plugin install, create a task name that will perform the task when called upon
+gulp.task('task-name',function () {
+    //do something eg. console.log('Hello World');
+});
