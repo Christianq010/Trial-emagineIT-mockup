bec2526e4c556c3c3877a504c7b509edc368b5dd
Christiaan Quyn <christianq010@gmail.com>
2017-05-04 23:25:24 +0530

    feat: Add SASS to gulp dependencies via npm

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,17 @@
 //Create a variable named gulp to reference our gulp files in the node_modules folder when installed
 var gulp = require('gulp');
 
+// Variable to use SASS after our installation
+var sass = require('gulp-sass');
+
 //after each task/plugin install, create a task name that will perform the task when called upon
-gulp.task('task-name',function () {
+gulp.task('sass', function () {
     //do something eg. console.log('Hello World');
+        return gulp.src('./sass/*.scss')
+            .pipe(sass().on('error', sass.logError))
+            .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./sass/*.scss', ['sass']);
 });
